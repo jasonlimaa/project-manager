11301d33c51ee72baf8c154d43e413e7ea5c7ad9
ali <ali1995ez@gmail.com>
2022-10-26 22:56:05 +0330

    update user profile

--- app/http/controllers/user.controller.js
+++ app/http/controllers/user.controller.js
@@ -1,3 +1,5 @@
+const { UserModel } = require("../../models/user");
+
 class UserController {
   getProfile(req, res, next) {
     try {
@@ -11,7 +13,33 @@ class UserController {
       next(error);
     }
   }
-  editProfile() {}
+  async editProfile(req, res, next) {
+    try {
+      let data = { ...req.body };
+      const fields = ["first_name", "last_name", "skills"];
+      const badValues = ["", " ", ".", null, undefined, 0, -1, NaN, [], {}];
+      const username = req.user.username;
+      Object.entries(data).forEach(([key, value]) => {
+        if (badValues.includes(value)) {
+          delete data[key];
+        }
+        if (!fields.includes(key)) {
+          delete data[key];
+        }
+      });
+      const result = await UserModel.updateOne({ username }, { $set: data });
+      if (result.modifiedCount > 0) {
+        return res.status(200).json({
+          success: true,
+          status: 200,
+          message: "profile updated successfully",
+        });
+      }
+      throw { status: 400, message: "updated not success" };
+    } catch (error) {
+      next(error);
+    }
+  }
   addSkills() {}
   editSkills() {}
   acceptInviteInTeam() {}
