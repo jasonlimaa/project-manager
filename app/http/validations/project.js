a895cde1c8f64cd04cba65c1addb263854bdc723
ali <ali1995ez@gmail.com>
2022-10-29 15:22:22 +0330

    create project add tags

--- app/http/validations/project.js
+++ app/http/validations/project.js
@@ -3,6 +3,7 @@ const { body } = require("express-validator");
 function createProjectValidator() {
   return [
     body("title").notEmpty().withMessage("Title is required"),
+    body("tags").isLength({ min: 0, max: 10 }).withMessage("max tags number is 10"),
     body("text").notEmpty().isLength({ min: 20 }).withMessage("at least 25 chars for text required"),
   ];
 }
--- app/modules/express-fileupload.js
+++ app/modules/express-fileupload.js
@@ -7,7 +7,6 @@ const uploadfile = async (req, res, next) => {
     const imagePath = path.join(createUploadPath(), Date.now() + path.extname(image.name));
     req.body.image = imagePath;
     let uploadPath = path.join(__dirname, "..", "..", imagePath);
-    console.log(uploadPath);
     image.mv(uploadPath, (err) => {
       if (err) throw { status: 500, message: "upload image not successful" };
       next();
